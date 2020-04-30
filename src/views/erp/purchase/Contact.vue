fd957aa39801783e35e15782e9b32eee91fbc3f1
yangsiyu0016 <969806654@qq.com>
2020-04-30 17:03:10 +0800

    添加供应商管理模块

---
+++ src/views/erp/purchase/Contact.vue
@@ -0,0 +1,173 @@
+<template>
+    <div>
+        <el-button size="mini" type="primary" @click="showAddContactView"  style="float: left">添加联系人</el-button>
+        <el-table :data="contacts">
+            <el-table-column type="index" width="30"></el-table-column>
+            <el-table-column prop="name" label="姓名" ></el-table-column>
+            <el-table-column prop="supplyCall" label="称呼"> </el-table-column>
+            <el-table-column label="性别">
+                <template slot-scope="scope">
+                    <span v-if="scope.row.sex==1">男</span>
+                    <span v-else>女</span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="position" label="职位"></el-table-column>
+            <el-table-column prop="telephone" label="座机"></el-table-column>
+            <el-table-column prop="callphone" label="手机"></el-table-column>
+            <el-table-column prop="email" label="邮箱"></el-table-column>
+            <el-table-column prop="birthday" label="生日"></el-table-column>
+            <el-table-column prop="remarks" label="备注"></el-table-column>
+            <el-table-column
+                    label="操作">
+                <template slot-scope="scope">
+                    <el-button @click="showEditContactView(scope.row)" type="primary" size="mini" style="padding: 3px 4px 3px 4px;margin: 2px">编辑</el-button>
+                    <el-button @click="deleteContact(scope.row)" type="danger"  size="mini"  style="padding: 3px 4px 3px 4px;margin: 2px">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-dialog title="添加联系人" :visible.sync="ContactDialogVisible" :append-to-body="true" :close-on-click-modal="false">
+            <linkman-form :isEdit="isEdit" @editLinkman="editContact" :oldContact="oldContact" @addContact="addContact"></linkman-form>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+    import ContactForm from "@/views/erp/purchase/ContactForm";
+    export default {
+        name: "Contact",
+        components: {ContactForm},
+        props: {
+            contacts: {
+                type: Array,
+                default: () => {}
+            },
+            suppilerId: {
+                type: String,
+                default: ''
+            }
+        },
+        methods: {
+            deleteContact(row){
+                this.contact.some((item, i) => {
+                    if (item == row) {
+                        this.contact.splice(i, 1);
+                        return true;
+                    }
+                })
+            },
+            editContact(contact) {
+                let  _this = this;
+                if (_this.suppilerId) {
+                    _this.putNoEnCodeRequest('/erp/contact/updateContact', contact).then((resp) => {
+                        if(resp.data.status == '200') {
+                            _this.contacts.some((item, i) => {
+                                if (item == _this.oldContact) {
+                                    _this.contacts.splice(i, 1, contact);
+                                }
+                            })
+                            _this.contactDialogVisible = false;
+                            _this.$message.success("更新成功");
+                        }else {
+                            _this.$message.error("更新失败");
+                        }
+                    })
+                }else {
+                    _this.contacts.some((item, i) => {
+                        if(item == _this.oldContact) {
+                            _this.contacts.splice(i, 1, contact);
+                        }
+                    })
+                    this.contactDialogVisible = false;
+                }
+            },
+            addContact(contact){
+                if(this.suppilerId){
+                    Object.assign(contact,{supplierId: this.supplierId});
+                    this.postNoEnCodeRequest('/erp/contact/addContact', contact).then((resp) => {
+                        if(resp.data.status == '200') {
+                            contact = resp.data.contact;
+                            this.contacts.push(contact);
+                            this.contactDialogVisible = false;
+                            this.$message.success("添加成功");
+                        }else {
+                            this.$message.error("添加失败");
+                        }
+                    })
+                }else {
+                    this.contacts.push(contact);
+                    this.contactDialogVisible = false;
+                }
+            },
+
+            showEditContactView(row) {
+                this.isEdit = true;
+                this.oldContact = row;
+                this.contactDialogVisible = true;
+            },
+
+            showAddContactView() {
+                this.isEdit = false;
+                this.oldContact = {
+                    name: '',
+                    supplyCall: '',
+                    sex: '1',
+                    position: '',
+                    telephone: '',
+                    callphone: '',
+                    email: '',
+                    birthday: '',
+                    remarks: ''
+                };
+                this.contactDialogVisible = true;
+            }
+        },
+        data(){
+            return{
+                isEdit:false,
+                oldContact:{
+                    name: '',
+                    supplyCall: '',
+                    sex: '1',
+                    position: '',
+                    telephone: '',
+                    callphone: '',
+                    email: '',
+                    birthday: '',
+                    remarks: ''
+                },
+                contactDialogVisible:false,
+
+                //日期选择器
+                pickerOptions: {
+                    disabledDate(time) {
+                        return time.getTime() > Date.now();
+                    },
+                    shortcuts: [{
+                        text: '今天',
+                        onClick(picker) {
+                            picker.$emit('pick', new Date());
+                        }
+                    }, {
+                        text: '昨天',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24);
+                            picker.$emit('pick', date);
+                        }
+                    }, {
+                        text: '一周前',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', date);
+                        }
+                    }]
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/erp/purchase/Supplier.vue
+++ src/views/erp/purchase/Supplier.vue
@@ -1,10 +1,107 @@
 <template>
-    <div>供应商管理</div>
+    <div>
+        <el-container>
+            <el-header style="padding: 0px;display:flex;justify-content:space-between;align-items: center">
+                <div>
+                    <el-input clearable prefix-icon="el-icon-search" style="width: 300px;margin: 0px;padding: 0px;" size="mini" placeholder="通过供应商名称搜索客户,记得回国呦……"></el-input>
+                </div>
+                <div style="margin-left: 5px;margin-right: 20px;display: inline">
+                    <el-button type="primary" size="mini" icon="el-icon-plus" @click="showAddSupplierView">添加供应商</el-button>
+                </div>
+            </el-header>
+            <el-main style="padding-left: 0px;padding-top: 0px">
+                <el-table :data="suppliers" size="mini" style="width:100%">
+                    <el-table-column type="index" width="50"></el-table-column>
+                    <el-table-column prop="gtime" label="获取时间"></el-table-column>
+                    <el-table-column prop="supplierName" label="名称"></el-table-column>
+                    <el-table-column label="区域">
+                        <el-table-column prop="country.name" label="国家"></el-table-column>
+                        <el-table-column prop="province.name" label="省份"></el-table-column>
+                        <el-table-column prop="city.name" label="城市"></el-table-column>
+                        <el-table-column prop="county.name" label="地区"></el-table-column>
+                    </el-table-column>
+                    <el-table-column prop="owner.realName" label="拥有人"></el-table-column>
+                    <el-table-column prop="systemUser.realName" label="创建人"></el-table-column>
+                    <el-table-column prop="createTime" label="创建时间"></el-table-column>
+                    <el-table-column
+                            label="操作">
+                        <template slot-scope="scope" >
+                            <el-button @click="showEditSupplierView(scope.row)"  type="primary"  size="mini" style="padding: 3px 4px 3px 4px;margin: 2px">编辑</el-button>
+                            <el-button type="danger" @click="deleteSupplier(scope.row)"  size="mini"   style="padding: 3px 4px 3px 4px;margin: 2px">删除</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </el-main>
+        </el-container>
+        <el-dialog :visible.sync="dialogVisible" :title="dialogTitle" :close-on-click-modal="false" width="80%">
+            <supplier-form :isEdit="isEdit" :oldSupplier="oldSupplier" @close="closeWin" @editSuccess="editSuccess"></supplier-form>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
+
+    import SupplierForm from "@/views/erp/purchase/SupplierForm";
     export default {
-        name: "Supplier"
+        name: "Supplier",
+        components: {SupplierForm},
+        mounted() {
+            this.loadSupplier();
+        },
+        data() {
+            return {
+                suppliers: [],
+                currentPage: 1,
+                totalCount: -1,
+                dialogVisible: false,
+                dialogTitle: '',
+                oldSupplier: {},
+                isEdit: false
+            }
+        },
+        methods: {
+            //s删除功能
+            deleteSupplier(supplier){
+                console.log(supplier.id);
+                this.$message.info("暂时不考虑实现删除")
+            },
+            editSuccess() {
+                this.closeWin();
+                this.loadSupplier();
+            },
+            closeWin(){
+                this.dialogVisible = false;
+            },
+            //加载供货商管理页面数据
+            loadSupplier() {
+                this.getRequest('/erp/supplier/page?page=' + this.currentPage + '&size=10').then((resp) => {
+                    this.suppliers = resp.data.suppliers;
+                    this.totalCount = resp.data.count;
+                })
+            },
+            //数据编辑页
+            showEditSupplierView(supplier) {
+                this.oldSupplier = supplier;
+                this.dialogTitle = '编辑供货商';
+                this.dialogVisible = true;
+                this.isEdit = true;
+            },
+            showAddSupplierView(supplier) {
+                this.oldSupplier = {
+                    supplierName: '',
+                    area: [],
+                    gtime: '',
+                    owner: {
+                        realName: ''
+                    },
+                    contacts: [],
+                    supplyAddresses: []
+                };
+                this.isEdit = false;
+                this.dialogTitle = '新增供货商';
+                this.dialogVisible = true;
+            }
+        }
     }
 </script>
 
