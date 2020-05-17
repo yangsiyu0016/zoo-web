2c8afe4b380f6c0b9fb50415e199adce05569760
525475256 <525475256@qq.com>
2020-05-17 13:37:30 +0800

    回车键登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,10 +4,10 @@
                  label-width="0px" v-loading="loading">
             <h3 class="login_title">系统登录</h3>
             <el-form-item prop="username">
-                <el-input  v-model="loginForm.username" ></el-input>
+                <el-input @keyup.enter.native="submitClick"  v-model="loginForm.username" ></el-input>
             </el-form-item>
             <el-form-item prop="password">
-                <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
+                <el-input @keyup.enter.native="submitClick" type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
             </el-form-item>
             <el-checkbox class="login_remember" v-model="checked" label-position="left">记住密码</el-checkbox>
             <el-form-item style="width: 100%">
@@ -20,8 +20,16 @@
 <script>
     export default {
         name: "index",
+        mounted(){
+            let _this = this;
+            document.onkeydown = function(e){
+                if(e.key=="Enter"){
+                    _this.submitClick();
+                }
+            }
+        },
         methods:{
-            submitClick:function(){
+            submitClick(){
                 let _this = this;
                 _this.loading = true;
                 _this.postRequest('/auth',this.loginForm).then(resp=>{
