c7a457608b7aa926cbf402a4ba24af2ac82f7747
yangsiyu0016 <969806654@qq.com>
2020-06-19 10:03:07 +0800

    添加产品组装拆分页面

---
+++ src/views/erp/stock/productcompose/ProductComposeForm.vue
@@ -0,0 +1,214 @@
+<template>
+    <div>
+        <el-form size="mini"   ref="productForm" label-width="150px">
+            <el-card shadow="hover">
+                <div slot="header" class="clearfix">
+                    <span style="float: left;">组装产品信息</span>
+                </div>
+                <div>
+                    <el-row :gutter="20">
+                        <el-col :span="8">
+                            <el-form-item prop="initDate"  label="单据日期:">
+                                <el-date-picker
+                                        style="float: left; "
+                                        align="right"
+                                        type="date"
+                                        size="mini"
+                                        v-model="productCompose.initDate"
+                                        placeholder="选择日期"
+                                        value-format="yyyy-MM-dd"
+                                        format="yyyy 年 MM 月 dd 日"
+                                        :picker-options="pickerOptions">
+                                </el-date-picker>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="8" >
+                            <el-form-item label="单号录入方式:">
+                                <el-select size="mini" v-model="productCompose.codeGeneratorType">
+                                    <el-option key="AUTO" label="自动生成" value="AUTO"></el-option>
+                                    <el-option key="SELF" label="手动录入" value="SELF"></el-option>
+                                </el-select>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="8" v-show="productCompose.id||productCompose.codeGeneratorType=='SELF'">
+                            <el-form-item label="单号:" prop="code" :required="productCompose.codeGeneratorType=='SELF'">
+                                <el-input size="mini" v-model="productCompose.code" :disabled="isEdit"></el-input>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="8" >
+                            <el-form-item label="组装仓库:">
+                                <el-select size="mini" value-key="id" v-model="productCompose.warehouse">
+                                    <el-option v-for="(item,i) in warehouses" :key="item.id" :label="item.name" :value="item"></el-option>
+                                </el-select>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+                    <el-row :gutter="20">
+                        <el-col :span="10">
+                            <el-form-item label="产品编号:">
+                                <el-input
+                                        style="width: 300px; float: left"
+                                        size="mini"
+                                        v-model="productCompose.product.code">
+                                </el-input>
+                                <el-button style="margin-top: 0px;float: left" size="mini" type="success" @click="selectProduct">选择</el-button>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="8">
+                            <el-form-item label="产品名称:">
+                                <el-input
+                                        style="width: 300px; float: left"
+                                        size="mini"
+                                        v-model="productCompose.product.name">
+                                </el-input>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+                    <el-row :gutter="20">
+                        <el-col :span="8">
+                            <el-form-item label="备注:" prop="description">
+                                <el-input type="textarea" v-model="productCompose.description"></el-input>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+                </div>
+            </el-card>
+            <el-card shadow="hover">
+                <div slot="header" class="clearfix">
+                    <span style="float: left;">产品清单</span>
+                </div>
+                <div>
+                    <el-button style="margin-top: 0px;float: left" size="mini" type="success" @click="selectProduct">选择材料</el-button>
+                </div>
+                <el-table
+                        :data="productCompose.product"
+                        size="mini"
+                        style="width:100%" >
+                    <el-table-column
+                            type="selection"
+                            align="left"
+                            width="80">
+                    </el-table-column>
+                    <el-table-column label="产品编号" prop="code" ></el-table-column>
+                    <el-table-column label="产品名称" prop="name" ></el-table-column>
+                    <el-table-column prop="typeName" align="left" width="100" label="分类"></el-table-column>
+                    <el-table-column prop="productBrand.name" align="left"  label="品牌" ></el-table-column>
+
+                    <el-table-column prop="spec" align="left" label="规格"></el-table-column>
+                    <el-table-column prop="unit.name" align="left" label="单位"></el-table-column>
+                    <el-table-column prop="weight" align="left" label="重量"></el-table-column>
+                    <el-table-column prop="color" align="left" label="颜色"></el-table-column>
+                    <el-table-column prop="puse" align="left" label="用途"></el-table-column>
+                    <el-table-column prop="description" align="left" label="备注"></el-table-column>
+
+                    <!--<el-table-column label="数量" prop="number"></el-table-column>
+                    <el-table-column label="价格" prop="price"></el-table-column>
+                    <el-table-column label="总额" prop="totalMoney"></el-table-column>-->
+                    <el-table-column
+                            label="操作" width="120">
+                        <template slot-scope="scope">
+                            <el-button  type="primary" @click="showEditProductView(scope.row)"  style="padding: 3px 4px 3px 4px;margin: 2px">编辑</el-button>
+                            <el-button type="danger"  @click="deleteDetail(scope.row)" style="padding: 3px 4px 3px 4px;margin: 2px">删除</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </el-card>
+            <el-card shadow="hover">
+                <div style="text-align: center">
+                    <el-button @click="save" type="primary" size="mini">保存</el-button>
+                    <el-button @click="cancel" type="info" size="mini">取消</el-button>
+                </div>
+            </el-card>
+        </el-form>
+        <product-dialog @closeWin="closeProductDialog" :visible="proDialogVisible" :title="proDialogTitle" @dblclick="proDblclick"></product-dialog>
+    </div>
+</template>
+
+<script>
+
+    import ProductDialog from "@/components/dialog/ProductDialog";
+
+    export default {
+        name: "ProductComposeForm",
+        components: {ProductDialog},
+        mounted() {
+            this.loadWarehouse();
+        },
+        data() {
+            return {
+                isEdit: false,
+                proDialogTitle: '',
+                proDialogVisible: false,
+                productCompose: {
+                    product: {
+                        code: ''
+                    }
+                },
+                warehouses:[],
+                //日期选择器
+                pickerOptions: {
+                    disabledDate(time) {
+                        return time.getTime() > Date.now();
+                    },
+                    shortcuts: [{
+                        text: '今天',
+                        onClick(picker) {
+                            picker.$emit('pick', new Date());
+                        }
+                    }, {
+                        text: '昨天',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24);
+                            picker.$emit('pick', date);
+                        }
+                    }, {
+                        text: '一周前',
+                        onClick(picker) {
+                            const date = new Date();
+                            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', date);
+                        }
+                    }]
+                }
+            }
+        },
+        methods: {
+            save() {
+
+            },
+            cancel() {
+
+            },
+            showEditProductView() {
+
+            },
+            deleteDetail() {
+
+            },
+            loadWarehouse() {
+                this.getRequest('/erp/warehouse/all').then(resp=> {
+                    if (resp &&resp.data) {
+                        this.warehouses = resp.data;
+                    }
+                })
+            },
+            closeProductDialog() {
+                this.proDialogVisible= false;
+            },
+            proDblclick(row) {
+                this.productCompose.product = row;
+                //this.productSplit.product.name = row.name;
+                this.closeProductDialog();
+            },
+            selectProduct() {
+                this.proDialogVisible = true;
+                this.proDialogTitle = '选择产品';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/erp/stock/productsplit/List.vue
@@ -0,0 +1,149 @@
+<template>
+    <div>
+        <el-form>
+            <el-card>
+                <div slot="header" class="clearfix">
+                    <span style="float: left;">查询条件</span>
+                </div>
+                <el-row :gutter="20">
+                    <el-col :span="8" >
+                        <el-form-item prop=""  label="拆分日期:">
+                            <el-date-picker
+                                    style="float: left"
+                                    align="right"
+                                    type="daterange"
+                                    range-separator="至"
+                                    start-placeholder="开始日期"
+                                    end-placeholder="结束日期"
+                                    size="mini"
+                                    v-model="searchDate"
+                                    value-format="yyyy-MM-dd"
+                                    format="yyyy 年 MM 月 dd 日"
+                                    :picker-options="pickerOptions">
+                            </el-date-picker>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+                <el-button style="margin-top: 6px;float: left" size="mini" type="success" @click="search">查询</el-button>
+            </el-card>
+        </el-form>
+        <el-card>
+            <el-main style="padding-left: 0px;padding-top: 0px">
+                <div class="export" style="float: left">
+                    <el-button @click="showAddProductSplitViews" style="margin-top: 2px;" size="medium" type="success">添加</el-button>
+                </div>
+                <el-table :data="productSplits" tooltip-effect="dark" ref="multipleTable" style="width: 100%" id="table" >
+                    <el-table-column
+                            type="selection"
+                            width="35">
+                    </el-table-column>
+                    <el-table-column type="index" width="5px">
+                        <template slot-scope="scope" >
+                            <span>{{(currentPage - 1) * 10 + scope.$index + 1}}</span>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="" label="拆分单编号"></el-table-column>
+                    <el-table-column prop="" label="拆分仓库"></el-table-column>
+                    <el-table-column prop="" label="拆分日期"></el-table-column>
+                    <el-table-column prop="" label="商品编号"></el-table-column>
+                    <el-table-column prop="" label="商品名称"></el-table-column>
+                    <el-table-column prop="" label="商品类型"></el-table-column>
+                    <el-table-column prop="" label="拆分后数量"></el-table-column>
+                    <el-table-column prop="" label="批号"></el-table-column>
+                    <el-table-column prop="" label="拆分人"></el-table-column>
+                    <el-table-column prop="" label="创建时间"></el-table-column>
+                    <el-table-column prop="" label="结束时间"></el-table-column>
+                    <el-table-column prop="status" label="状态">
+                        <template slot-scope="scope">
+                            <el-tag v-if="scope.row.status=='WTJ'" type="info" size="mini" effect="dark">未提交</el-tag>
+                            <el-tag v-if="scope.row.status=='DESTROY'"  type="info" size="mini" effect="dark">已作废</el-tag>
+                            <el-tag v-if="scope.row.status=='FINISHED'"  type="success" size="mini" effect="dark">订单完成</el-tag>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="" label="备注"></el-table-column>
+                </el-table>
+                <el-pagination
+                        background
+                        :page-size="10"
+                        @current-change="currentChange"
+                        :current-page="currentPage"
+                        layout="prev,pager,next"
+                        :total="totalCount">
+                </el-pagination>
+            </el-main>
+        </el-card>
+        <el-dialog :visible.sync="dialogVisible" :title="dialogTitle" :close-on-click-modal="false" :append-to-body="true" width="77%">
+            <product-split-form   @close="closeDialog" @callback="callback"></product-split-form>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+    import ProductSplitForm from "./ProductSplitForm";
+    export default {
+        name: "List",
+        components: {ProductSplitForm},
+        data() {
+            return {
+                dialogVisible:false,
+                dialogTitle: '',
+                productSplits: [],
+                currentPage: 1,
+                totalCount: -1,
+                searchDate:[],
+                pickerOptions: {
+                    disabledDate(time) {
+                        return time.getTime() > Date.now();
+                    },
+                    shortcuts: [{
+                        text: '最近一周',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }, {
+                        text: '最近一个月',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }, {
+                        text: '最近三个月',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }]
+                },
+            }
+        },
+        methods: {
+            callback() {
+                this.dialogVisible = false;
+            },
+            closeDialog() {
+                  this.dialogVisible = false;
+            },
+            search() {
+
+            },
+            currentChange() {
+
+            },
+            showAddProductSplitViews() {
+                this.dialogVisible = true;
+                this.dialogTitle = "添加商品组装";
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
