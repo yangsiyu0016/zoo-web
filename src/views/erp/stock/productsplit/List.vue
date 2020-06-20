ba033f9d5498aeab9b62faf6b98515f154862b12
yangsiyu0016 <969806654@qq.com>
2020-06-20 17:31:35 +0800

    修改产品拆分页面

--- src/views/erp/stock/productsplit/List.vue
+++ src/views/erp/stock/productsplit/List.vue
@@ -1,37 +1,52 @@
 <template>
     <div>
-        <el-form>
-            <el-card>
-                <div slot="header" class="clearfix">
-                    <span style="float: left;">查询条件</span>
+        <el-container>
+            <el-header style="padding: 0px;display:flex;justify-content:space-between;align-items: center">
+                <div style="display: inline">
+                    <el-input size="mini" placeholder="通过产品名称搜索，记得回车呦..." clearable style="width: 300px;margin: 0px;padding: 0px;" prefix-icon="el-icon-search"></el-input>
+                    <el-button type="primary" size="mini" style="margin-left: 5px" icon="el-icon-search">搜索</el-button>
+                    <el-button slot="reference" type="primary" size="mini" style="margin-left: 5px"
+                               @click="showSearchView">
+                        <i class="fa fa-lg" style="margin-right: 5px"  v-bind:class="[searchViewVisible ? faangledoubleup:faangledoubledown]"></i>高级搜索
+                    </el-button>
+
                 </div>
-                <el-row :gutter="20">
-                    <el-col :span="8" >
-                        <el-form-item prop=""  label="拆分日期:">
-                            <el-date-picker
-                                    style="float: left"
-                                    align="right"
-                                    type="daterange"
-                                    range-separator="至"
-                                    start-placeholder="开始日期"
-                                    end-placeholder="结束日期"
-                                    size="mini"
-                                    v-model="searchDate"
-                                    value-format="yyyy-MM-dd"
-                                    format="yyyy 年 MM 月 dd 日"
-                                    :picker-options="pickerOptions">
-                            </el-date-picker>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
-                <el-button style="margin-top: 6px;float: left" size="mini" type="success" @click="search">查询</el-button>
-            </el-card>
-        </el-form>
-        <el-card>
+                <div style="margin-left: 5px;margin-right: 20px;display: inline">
+                    <el-button type="primary" size="mini" icon="el-icon-plus" @click="showAddView">添加</el-button>
+                </div>
+            </el-header>
             <el-main style="padding-left: 0px;padding-top: 0px">
-                <div class="export" style="float: left">
-                    <el-button @click="showAddProductSplitViews" style="margin-top: 2px;" size="medium" type="success">添加</el-button>
-                </div>
+                <transition name="slide-fade">
+                    <div v-show="searchViewVisible" style="margin-bottom: 10px;border: 1px;border-radius: 5px;border-style: solid;padding: 5px 0px 5px 0px;box-sizing:border-box;border-color: #20a0ff">
+                        <el-row :gutter="20" style="margin-top: 20px">
+                            <el-col :span="6">
+                                拆分单编号：<el-input size="mini" style="width: 200px" placeholder="拆分单编号" ></el-input>
+                            </el-col>
+                            <el-col :span="6">
+                                产品编号：<el-input size="mini" style="width: 200px" placeholder="产品编号" ></el-input>
+                            </el-col>
+                            <el-col :span="6">
+                                产品名称：<el-input size="mini" style="width: 200px" placeholder="产品名称" ></el-input>
+                            </el-col>
+                            <el-col :span="6">
+                                拆分仓库：<el-input size="mini" style="width: 200px" placeholder="拆分仓库" ></el-input>
+                            </el-col>
+
+                        </el-row>
+                        <el-row :gutter="20" style="margin-top: 20px">
+                            <el-col :span="6">
+                                拆分日期：<el-input size="mini" style="width: 200px" placeholder="拆分日期" ></el-input>
+                            </el-col>
+                            <el-col :span="6">
+                                创建日期：<el-input size="mini" style="width: 200px" placeholder="创建日期" ></el-input>
+                            </el-col>
+                            <el-col :span="5">
+                                <el-button icon="el-icon-zoom-out" size="mini" @click="cancelSearch">取消</el-button>
+                                <el-button icon="el-icon-search" type="primary" size="mini" >搜索</el-button>
+                            </el-col>
+                        </el-row>
+                    </div>
+                </transition>
                 <el-table :data="productSplits" tooltip-effect="dark" ref="multipleTable" style="width: 100%" id="table" >
                     <el-table-column
                             type="selection"
@@ -53,14 +68,14 @@
                     <el-table-column prop="" label="拆分人"></el-table-column>
                     <el-table-column prop="" label="创建时间"></el-table-column>
                     <el-table-column prop="" label="结束时间"></el-table-column>
-                    <el-table-column prop="status" label="状态">
+                    <el-table-column prop="" label="状态">
                         <template slot-scope="scope">
                             <el-tag v-if="scope.row.status=='WTJ'" type="info" size="mini" effect="dark">未提交</el-tag>
                             <el-tag v-if="scope.row.status=='DESTROY'"  type="info" size="mini" effect="dark">已作废</el-tag>
                             <el-tag v-if="scope.row.status=='FINISHED'"  type="success" size="mini" effect="dark">订单完成</el-tag>
                         </template>
                     </el-table-column>
-                    <el-table-column prop="" label="备注"></el-table-column>
+                    <el-table-column prop="" label="备注" :show-tooltip-when-overflow="true"></el-table-column>
                 </el-table>
                 <el-pagination
                         background
@@ -71,9 +86,9 @@
                         :total="totalCount">
                 </el-pagination>
             </el-main>
-        </el-card>
+        </el-container>
         <el-dialog :visible.sync="dialogVisible" :title="dialogTitle" :close-on-click-modal="false" :append-to-body="true" width="77%">
-            <product-split-form   @close="closeDialog" @callback="callback"></product-split-form>
+            <product-split-form   @close="closeDialog" :isEdit="isEdit" @callback="callback" :oldProductSplit="oldProductSplit"></product-split-form>
         </el-dialog>
     </div>
 </template>
@@ -85,12 +100,19 @@
         components: {ProductSplitForm},
         data() {
             return {
+                isEdit:false,
+                oldProductSplit:{
+
+                },
+                searchViewVisible:false,
                 dialogVisible:false,
                 dialogTitle: '',
                 productSplits: [],
                 currentPage: 1,
                 totalCount: -1,
                 searchDate:[],
+                faangledoubleup: 'fa-angle-double-up',
+                faangledoubledown: 'fa-angle-double-down',
                 pickerOptions: {
                     disabledDate(time) {
                         return time.getTime() > Date.now();
@@ -124,6 +146,12 @@
             }
         },
         methods: {
+            cancelSearch(){
+                this.searchViewVisible = false;
+            },
+            showSearchView() {
+                this.searchViewVisible = !this.searchViewVisible;
+            },
             callback() {
                 this.dialogVisible = false;
             },
@@ -136,9 +164,22 @@
             currentChange() {
 
             },
-            showAddProductSplitViews() {
+            showAddView() {
+                this.isEdit = false;
+                this.oldProductSplit = {
+                    splitTime: '',
+                    product: {
+                        code: ''
+                    },
+                    warehouse:{
+                        name:''
+                    },
+                    splitMaterials:[],
+                    codeGeneratorType:"AUTO",
+                    number:1
+                };
                 this.dialogVisible = true;
-                this.dialogTitle = "添加商品组装";
+                this.dialogTitle = "添加拆分单";
             }
         }
     }
